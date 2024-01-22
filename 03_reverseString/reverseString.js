d4ee342b6c1737a1fc5573726c6af94baf57e65f
kailermai <kailermaioff@gmail.com>
2024-01-22 20:03:45 +0800

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringArray = [];
+  let stringLen = string.length;
+  for (let i = 0; i < stringLen; i++) {
+    stringArray[i] = string[i];
+  }
+  let newStringArray = [];
+  for (let i = 0; i < stringLen; i++) {
+    newStringArray[i] = stringArray.pop();
+  }
+  return newStringArray.join("");
 };
 
 // Do not edit below this line
