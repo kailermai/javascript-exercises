c269fec954e4406a0d486e486b1df1e2b3912488
kailermai <kailermaioff@gmail.com>
2024-01-22 19:28:52 +0800

    Completed Exercise 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let returnstring = "";
+  if (num < 0) {
+    return "ERROR";
+  }
 
+  for (let i = 0; i < num; i++) {
+    returnstring += string;
+  }
+  return returnstring;
 };
 
 // Do not edit below this line
