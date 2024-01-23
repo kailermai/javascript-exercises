eda6268b973ef8513122634e386765612beeacbe
kailermai <kailermaioff@gmail.com>
2024-01-23 14:30:55 +0800

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+  for (const value of values) {
+    let indexValue = array.indexOf(value);
+    if (indexValue === -1) {
+      continue;
+    } else {
+      array.splice(indexValue, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
