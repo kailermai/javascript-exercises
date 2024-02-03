967a9e08e272e1e9b5c1bf239594a6cce2d93244
kailermai <kailermaioff@gmail.com>
2024-02-03 17:56:47 +0800

    Completed Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return (num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayNum) {
+	let total = 0;
+  arrayNum.forEach(num => {
+    total += num;
+  });
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arrayNum) {
+  let total = 1;
+  arrayNum.forEach(num => {
+    total *= num;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let total = num;
+	for (let i = 1; i < power; i++) {
+    total *= num;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1
+  }
+  else {
+    let total = 1;
+    for (let i = 2; i <= num; i++) {
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
