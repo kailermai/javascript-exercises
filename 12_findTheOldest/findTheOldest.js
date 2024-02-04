62734bc938cb6965e9531a6b592a14a28862e10f
kailermai <kailermaioff@gmail.com>
2024-02-05 00:47:26 +0800

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // const someAlive = people.some((person) => {
+    //     if (!person.hasOwnProperty('yearOfDeath')) {
+    //         return true;
+    //     }
+    // })
+
+
+    people.forEach(person => {
+        if (person.yearOfDeath){
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        }
+        else {
+            person.age = ((new Date()).getFullYear()) - person.yearOfBirth;
+        }
+    });
+    people.sort((a, b) => a.age > b.age ? 1 : -1);
+
+    return people[2];
 
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
